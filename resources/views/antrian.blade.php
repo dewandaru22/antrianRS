7647e3844255215a5a8b976d7ce978f85689a3d6
Riacup <supriyati98@mail.ugm.ac.id>
2020-07-10 23:31:49 +0700

    monitor

--- resources/views/antrian.blade.php
+++ resources/views/antrian.blade.php
@@ -1,29 +1,60 @@
 <html>
 <head>
-  <title>Sistem Antrian</title>
+  <title>Info Antrian</title>
   <link rel="stylesheet" href="https://bootstrap/css/bootstrap.min.css">  
   <link rel="stylesheet" href="https://>bootstrap/css/jquery-ui.min.css">  
   <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
-<div class="container-fluid alert-danger">
-<div class="row">
-    
-    @foreach ($data as $d)
-  <div class="col-md-4">
-    <div class="btn-danger" style="font-size: 20px;font-weight: bold;width: 100%;">{{ $d->nama_dokter }}</div>
-    <div class="text-lg-center btn-success img-thumbnail" style="font-size: 50px;font-weight: bold;width: 100%;">
-        {{ $d->antrian == null ? '' : $d->antrian->heads->nomor_periksa }}
-        <div class="btn-danger">{{$d->periksa->where('status', 'Menunggu')->count()}}</div>
-    </div>
-    <hr/>
-  </div>
-  @endforeach
+  <div class="container-fluid alert-danger">
+    <div class="row">
+   
+      <div class="col-md-3">
+        <div style="margin-top:20px;">
+          <img class="navbar-brand-full" src="{{('/template/images/rs-islam-logo.png')}}" width="200" height="60" alt="rsiy logo">
+        </div>
+      </div>
 
-</div>
-</div>
+      <div class="col-md-3">
+      </div>
+      <div class="col-md-3">
+      </div>
+
+      <div class="col-md-3">
+        <div class="text-lg-center btn-success img-thumbnail" style="font-size: 20px;font-weight: bold; margin-bottom:10px; margin-top:10px; color:#fff;">
+          <?php echo date('l');?>, <?php echo date('d F Y');?><br/>
+          <div id="jam_sekarang"><?php echo date('H:m:i');?></div>
+        </div>
+      </div>
+      <br/>
+      <hr/>
+
+      @foreach ($data as $d)
+      <div class="col-md-4">
+        <div class="text-lg-center btn-success img-thumbnail" style="font-size: 25px;font-weight: bold; height:160px; margin-bottom:10px;">
+            {{ $d->nama_dokter }}
+            <div class="btn-danger" style="font-size: 60px;font-weight: bold; height:110px;">{{ $d->antrian == null ? '' : $d->antrian->heads->nomor_periksa }} </div>
+        </div>
+      </div>
+      @endforeach
+
+      <div class="col-md-12">
+        <div class="text-lg-center btn-success img-thumbnail" style="font-size: 23px;font-weight: regular; margin-bottom:10px; color:#fff;">
+          <marquee>SELAMAT DATANG DI RUMAH SAKIT ISLAM YOGYAKARTA PDHI - Jl. Jogja Solo KM.12,5, Kringinan, Tirtomartani, Kec. Kalasan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55571</marquee>
+        </div>
+      </div>
+
+    </div>
+  </div>
 <script src="https://bootstrap/js/jquery.min.js"></script>
 <script src="https://bootstrap/js/jquery-ui.min.js"></script>
+<script>
+    var myVar = setInterval(myTimer, 1000);
+    function myTimer() {
+        var d = new Date();
+        document.getElementById("jam_sekarang").innerHTML = d.toLocaleTimeString();
+    }  
+</script>
 
 </body>
 </html>
